7d199b6f50aae50a7bb76928c52e290f0a428fc1
Sasha Ogurtov <sasha@surfly.com>
2022-10-12 00:14:11 +0200

    fix auth request parsing

--- src/auth/data.go
+++ src/auth/data.go
@@ -1,11 +1,12 @@
 package main
 
 type Athlete struct {
+	ID int `json:"id"`
 }
 
 type TokenResponse struct {
 	AccessToken  string  `json:"access_token"`
-	ExpiresAt    string  `json:"expires_at"`
+	ExpiresAt    int     `json:"expires_at"`
 	ExpiresIn    int     `json:"expires_in"`
 	RefreshToken string  `json:"refresh_token"`
 	TokenType    string  `json:"token_type"`
